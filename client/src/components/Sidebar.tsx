247f98978ad685e5eeaaccbe2f11a101b27fe4de
Anderson <avictoral@gmail.com>
2023-12-11 18:05:19 -0500

    updated Sidebar component

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,200 +1,200 @@
 /* eslint-disable react/prop-types */
 import {
-    Box,
-    Divider,
-    Drawer,
-    IconButton,
-    List,
-    ListItem,
-    ListItemButton,
-    ListItemIcon,
-    ListItemText,
-    Typography,
-    useTheme,
-  } from "@mui/material";
-  
-  import {
-    SettingsOutlined,
-    ChevronLeft,
-    ChevronRightOutlined,
-    HomeOutlined,
-    ShoppingCartOutlined,
-    Groups2Outlined,
-    ReceiptLongOutlined,
-    PublicOutlined,
-    TodayOutlined,
-    CalendarMonthOutlined,
-    AdminPanelSettingsOutlined,
-    TrendingUpOutlined,
-    PointOfSaleOutlined,
-    PieChartOutlined,
-  } from "@mui/icons-material";
-  import { useEffect, useState } from "react";
-  import { useLocation, useNavigate } from "react-router-dom";
-  import FlexBetween from "./FlexBetween";
+  Box,
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+  useTheme,
+} from "@mui/material";
+
+import {
+  SettingsOutlined,
+  ChevronLeft,
+  ChevronRightOutlined,
+  HomeOutlined,
+  ShoppingCartOutlined,
+  Groups2Outlined,
+  ReceiptLongOutlined,
+  PublicOutlined,
+  TodayOutlined,
+  CalendarMonthOutlined,
+  AdminPanelSettingsOutlined,
+  TrendingUpOutlined,
+  PointOfSaleOutlined,
+  PieChartOutlined,
+} from "@mui/icons-material";
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import FlexBetween from "./FlexBetween";
 //   import profileImage from "../assets/profile.jpeg";
-  
-  const navItems = [
-    {
-      text: "Dashboard",
-      icon: <HomeOutlined />,
-    },
-    {
-      text: "Client Facing",
-      icon: null,
-    },
-    {
-      text: "Products",
-      icon: <ShoppingCartOutlined />,
-    },
-    {
-      text: "Customers",
-      icon: <Groups2Outlined />,
-    },
-    {
-      text: "Transactions",
-      icon: <ReceiptLongOutlined />,
-    },
-    {
-      text: "Geography",
-      icon: <PublicOutlined />,
-    },
-    {
-      text: "Sales",
-      icon: null,
-    },
-    {
-      text: "Overview",
-      icon: <PointOfSaleOutlined />,
-    },
-    {
-      text: "Daily",
-      icon: <TodayOutlined />,
-    },
-    {
-      text: "Monthly",
-      icon: <CalendarMonthOutlined />,
-    },
-    {
-      text: "Breakdown",
-      icon: <PieChartOutlined />,
-    },
-    {
-      text: "Management",
-      icon: null,
-    },
-    {
-      text: "Admin",
-      icon: <AdminPanelSettingsOutlined />,
-    },
-    {
-      text: "Performance",
-      icon: <TrendingUpOutlined />,
-    },
-  ];
-  
-  const Sidebar = ({
-    user,
-    drawerWidth,
-    isSidebarOpen,
-    setIsSidebarOpen,
-    isNonMobile,
-  }) => {
-    const { pathname } = useLocation();
-    const [active, setActive] = useState("");
-    const navigate = useNavigate();
-    const theme = useTheme();
-  
-    useEffect(() => {
-      setActive(pathname.substring(1));
-    }, [pathname]);
-  
-    return (
-      <Box component="nav">
-        {isSidebarOpen && (
-          <Drawer
-            open={isSidebarOpen}
-            onClose={() => setIsSidebarOpen(false)}
-            variant="persistent"
-            anchor="left"
-            sx={{
+
+const navItems = [
+  {
+    text: "Dashboard",
+    icon: <HomeOutlined />,
+  },
+  {
+    text: "Client Facing",
+    icon: null,
+  },
+  {
+    text: "Products",
+    icon: <ShoppingCartOutlined />,
+  },
+  {
+    text: "Customers",
+    icon: <Groups2Outlined />,
+  },
+  {
+    text: "Transactions",
+    icon: <ReceiptLongOutlined />,
+  },
+  {
+    text: "Geography",
+    icon: <PublicOutlined />,
+  },
+  {
+    text: "Sales",
+    icon: null,
+  },
+  {
+    text: "Overview",
+    icon: <PointOfSaleOutlined />,
+  },
+  {
+    text: "Daily",
+    icon: <TodayOutlined />,
+  },
+  {
+    text: "Monthly",
+    icon: <CalendarMonthOutlined />,
+  },
+  {
+    text: "Breakdown",
+    icon: <PieChartOutlined />,
+  },
+  {
+    text: "Management",
+    icon: null,
+  },
+  {
+    text: "Admin",
+    icon: <AdminPanelSettingsOutlined />,
+  },
+  {
+    text: "Performance",
+    icon: <TrendingUpOutlined />,
+  },
+];
+
+const Sidebar = ({
+  user,
+  drawerWidth,
+  isSidebarOpen,
+  setIsSidebarOpen,
+  isNonMobile,
+}) => {
+  const { pathname } = useLocation();
+  const [active, setActive] = useState("");
+  const navigate = useNavigate();
+  const theme = useTheme();
+
+  useEffect(() => {
+    setActive(pathname.substring(1));
+  }, [pathname]);
+
+  return (
+    <Box component="nav">
+      {isSidebarOpen && (
+        <Drawer
+          open={isSidebarOpen}
+          onClose={() => setIsSidebarOpen(false)}
+          variant="persistent"
+          anchor="left"
+          sx={{
+            width: drawerWidth,
+            "& .MuiDrawer-paper": {
+              color: theme.palette.secondary[200],
+              backgroundColor: theme.palette.background.default[700],
+              boxSizing: "border-box",
+              borderWidth: isNonMobile ? 0 : "2px",
               width: drawerWidth,
-              "& .MuiDrawer-paper": {
-                color: theme.palette.secondary[200],
-                backgroundColor: theme.palette.background.alt,
-                boxSizing: "border-box",
-                borderWidth: isNonMobile ? 0 : "2px",
-                width: drawerWidth,
-              },
-            }}
-          >
-            <Box width="100%">
-              <Box m="1.5rem 2rem 2rem 3rem">
-                <FlexBetween color={theme.palette.secondary.main}>
-                  <Box display="flex" alignItems="center" gap="0.5rem">
-                    <Typography variant="h4" fontWeight="bold">
-                      ADMINDASH
-                    </Typography>
-                  </Box>
-                  {!isNonMobile && (
-                    <IconButton onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
-                      <ChevronLeft />
-                    </IconButton>
-                  )}
-                </FlexBetween>
-              </Box>
-              <List>
-                {navItems.map(({ text, icon }) => {
-                  if (!icon) {
-                    return (
-                      <Typography key={text} sx={{ m: "2.25rem 0 1rem 3rem" }}>
-                        {text}
-                      </Typography>
-                    );
-                  }
-                  const lowerText = text.toLowerCase();
-  
+            },
+          }}
+        >
+          <Box width="100%">
+            <Box m="1.5rem 2rem 2rem 3rem">
+              <FlexBetween color={theme.palette.primary.main}>
+                <Box display="flex" alignItems="center" gap="0.5rem">
+                  <Typography variant="h4" fontWeight="bold">
+                    ANDNATKR
+                  </Typography>
+                </Box>
+                {!isNonMobile && (
+                  <IconButton onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
+                    <ChevronLeft />
+                  </IconButton>
+                )}
+              </FlexBetween>
+            </Box>
+            <List>
+              {navItems.map(({ text, icon }) => {
+                if (!icon) {
                   return (
-                    <ListItem key={text} disablePadding>
-                      <ListItemButton
-                        onClick={() => {
-                          navigate(`/${lowerText}`);
-                          setActive(lowerText);
-                        }}
+                    <Typography key={text} sx={{ m: "2.25rem 0 1rem 3rem" }}>
+                      {text}
+                    </Typography>
+                  );
+                }
+                const lowerText = text.toLowerCase();
+
+                return (
+                  <ListItem key={text} disablePadding>
+                    <ListItemButton
+                      onClick={() => {
+                        navigate(`/${lowerText}`);
+                        setActive(lowerText);
+                      }}
+                      sx={{
+                        backgroundColor:
+                          active === lowerText
+                            ? theme.palette.secondary[300]
+                            : "transparent",
+                        color:
+                          active === lowerText
+                            ? theme.palette.primary[600]
+                            : theme.palette.secondary[100],
+                      }}
+                    >
+                      <ListItemIcon
                         sx={{
-                          backgroundColor:
-                            active === lowerText
-                              ? theme.palette.secondary[300]
-                              : "transparent",
+                          ml: "2rem",
                           color:
                             active === lowerText
                               ? theme.palette.primary[600]
-                              : theme.palette.secondary[100],
+                              : theme.palette.secondary[200],
                         }}
                       >
-                        <ListItemIcon
-                          sx={{
-                            ml: "2rem",
-                            color:
-                              active === lowerText
-                                ? theme.palette.primary[600]
-                                : theme.palette.secondary[200],
-                          }}
-                        >
-                          {icon}
-                        </ListItemIcon>
-                        <ListItemText primary={text} />
-                        {active === lowerText && (
-                          <ChevronRightOutlined sx={{ ml: "auto" }} />
-                        )}
-                      </ListItemButton>
-                    </ListItem>
-                  );
-                })}
-              </List>
-            </Box>
-  
-            {/* <Box position="absolute" bottom="2rem">
+                        {icon}
+                      </ListItemIcon>
+                      <ListItemText primary={text} />
+                      {active === lowerText && (
+                        <ChevronRightOutlined sx={{ ml: "auto" }} />
+                      )}
+                    </ListItemButton>
+                  </ListItem>
+                );
+              })}
+            </List>
+          </Box>
+
+          {/* <Box position="absolute" bottom="2rem">
               <Divider />
               <FlexBetween textTransform="none" gap="1rem" m="1.5rem 2rem 0 3rem">
                 <Box
@@ -226,11 +226,10 @@ import {
                 />
               </FlexBetween>
             </Box> */}
-          </Drawer>
-        )}
-      </Box>
-    );
-  };
-  
-  export default Sidebar;
-  
+        </Drawer>
+      )}
+    </Box>
+  );
+};
+
+export default Sidebar;
