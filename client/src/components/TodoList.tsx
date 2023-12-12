1f17a33102f0abd4b7b93e3da7b1f91acf514c0b
Anderson <avictoral@gmail.com>
2023-12-12 11:54:11 -0500

    created TodoList component

---
+++ client/src/components/TodoList.tsx
@@ -0,0 +1,75 @@
+import { useState } from "react";
+import {
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Checkbox,
+  IconButton,
+  useTheme,
+  Box,
+  Divider,
+} from "@mui/material";
+
+import { DeleteOutlineOutlined } from "@mui/icons-material";
+
+type Props = {};
+
+const todoList = [
+  { id: 1, description: "Buy groceries", isChecked: false },
+  { id: 2, description: "Complete work assignment", isChecked: true },
+  { id: 3, description: "Go for a run", isChecked: false },
+  { id: 4, description: "Read a book", isChecked: true },
+];
+
+const TodoList = () => {
+  const theme = useTheme();
+  const [checked, setChecked] = useState(false);
+
+  const handleToggle = (index: number) => () => {
+    const isChecked = todoList[index].isChecked;
+    setChecked(!isChecked);
+  };
+  return (
+    <List
+      sx={{
+        width: "100%",
+        backgroundColor: theme.palette.background.paper,
+      }}
+    >
+      {todoList.map((task) => (
+        <ListItem
+          key={task.id}
+          secondaryAction={
+            <IconButton edge="end" aria-label="comments">
+              <DeleteOutlineOutlined />
+            </IconButton>
+          }
+          disablePadding
+        >
+          <ListItemButton
+            role={undefined}
+            onClick={handleToggle(task.id)}
+            dense
+          >
+            <ListItemIcon>
+              <Checkbox
+                edge="start"
+                checked={task.isChecked}
+                // onChange={handleToggle(task.id)}
+                tabIndex={-1}
+                disableRipple
+                inputProps={{ "aria-labelledby": task.description }}
+              />
+            </ListItemIcon>
+            <ListItemText id={`taskID ${task.id}`} primary={task.description} />
+          </ListItemButton>
+        </ListItem>
+      ))}
+      <Divider />
+    </List>
+  );
+};
+
+export default TodoList;
