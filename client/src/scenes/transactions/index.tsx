3ca42718849154cb38aba082d29958f64ab29a9b
Anderson <avictoral@gmail.com>
2023-12-14 06:37:58 -0500

    created Transaction component

---
+++ client/src/scenes/transactions/index.tsx
@@ -0,0 +1,110 @@
+import {
+  Box,
+  IconButton,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
+import Header from "../../components/Header";
+import FlexBetween from "../../components/FlexBetween";
+import { AddCircleOutlineOutlined } from "@mui/icons-material";
+import StatBox from "../../components/StatBox";
+import { DataGrid } from "@mui/x-data-grid";
+import { useState } from "react";
+import ModalTransactions from "./Modal";
+
+type Props = {};
+
+const Transactions = () => {
+  const theme = useTheme();
+  const isNonMediumScreens = useMediaQuery("(min-width: 1200px)");
+  //   const { data, isLoading } = useGetDashboardQuery();
+  const { data, isLoading } = useState(false);
+
+  const columns = [
+    {
+      field: "_id",
+      headerName: "ID",
+      flex: 1,
+    },
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 0.5,
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      flex: 1,
+    },
+    {
+      field: "phoneNumber",
+      headerName: "Phone Number",
+      flex: 0.5,
+      renderCell: (params) => {
+        return params.value.replace(/^(\d{3})(\d{3})(\d{4})/, "($1)$2-$3");
+      },
+    },
+    {
+      field: "country",
+      headerName: "Country",
+      flex: 0.4,
+    },
+    {
+      field: "occupation",
+      headerName: "Occupation",
+      flex: 1,
+    },
+    {
+      field: "role",
+      headerName: "Role",
+      flex: 0.5,
+    },
+  ];
+
+  return (
+    <Box m="1.5rem 2.5rem">
+       <FlexBetween>
+      <Header title="TRANSACTIONS" subtitle="List of Transactions" />
+      <ModalTransactions />
+        {/* <IconButton sx={{ mr: "1.5rem" }}>
+          <AddCircleOutlineOutlined sx={{ fontSize: "2rem" }} />
+        </IconButton> */}
+      </FlexBetween>
+      <Box
+        mt="40px"
+        height="75vh"
+        sx={{
+          "& .MuiDataGrid-root": { border: "none" },
+          "& .MuiDataGrid-cell": { borderBottom: "none" },
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: theme.palette.background.alt,
+            color: theme.palette.secondary[100],
+            borderBottom: "none"
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: theme.palette.background.alt,
+          },
+          "& .MuiDataGrid-footerContainer": {
+            backgroundColor: theme.palette.background.alt,
+            color: theme.palette.secondary[100],
+            borderTop: "none"
+          },
+          "& .MuiDataGrid-toolbarContainer .MuiButton-text":{
+            color: `${theme.palette.secondary[200]} !important`,
+          }
+        }}
+      >
+        <DataGrid
+          loading={false}
+        //   loading={isLoading || !data}
+          getRowId={(row) => row._id}
+          rows={data || []}
+          columns={columns}
+        />
+      </Box>
+    </Box>
+  );
+};
+
+export default Transactions;
